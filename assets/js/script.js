dbcaf1fbd704e3001dcd87fc7404a3aa1aadc425
Mehrdad Lashgari <mehr.lashgari@gmail.com>
2022-03-04 10:29:29 +0000

    saved

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const API_KEY="VZn-Fx2ohpPGHAs_RhP_m6IgxYA";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+
+const resultsModal=new bootstrap.Modal(document.getElementById('resultsModal'))
